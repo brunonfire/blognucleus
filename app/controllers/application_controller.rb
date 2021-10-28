2e836314baa85a680a6953610a5ebb2aff6ef04a
brunonfire <linkedx@gmail.com>
2021-10-28 07:57:29 -0300

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,37 @@
 class ApplicationController < ActionController::Base
+
+
+    protected
+
+    def current_user
+        @current_user ||= user.find_by_email_and_id(session[:email], session[:uid])
+    rescue
+        false
+    end
+
+#==========================================================================================================================================
+ # verifica se existe um usuario logado
+    def require_user
+        if current_user
+            true
+        else
+            redirect_to login_admin_users_url, alert: "Voce não tem acesso a essa area."
+            return false
+        end
+    end 
+ 
+
+ #===========================================================================================================================================
+ # verifica se o usuario logado é um admin
+    def admin?
+        if current_user.tipo == "admin"
+            true 
+        else
+            redirect_to login_admin_users_url, alert: "Você não tem acesso a essa area."
+            return false
+        end
+    end 
+
+
 end
+
